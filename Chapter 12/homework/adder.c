f7a07033aeac2ae3d91da26bc81e93108e1e5531
shima8823 <taiyaki1275@gmail.com>
2024-08-13 15:34:17 +0900

    add homeworks

---
+++ Chapter 12/homework/adder.c
@@ -0,0 +1,48 @@
+// cc 11_10_adder.c csapp.c -o cgi-bin/adder
+
+/*
+ * adder.c - a minimal CGI program that adds two numbers together
+ */
+/* $begin adder */
+#include "csapp.h"
+
+int main(void) {
+    char *buf, *p, *left_p, *right_p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1=0, n2=0;
+
+    /* Extract the two arguments */
+    if ((buf = getenv("QUERY_STRING")) != NULL) {
+	p = strchr(buf, '&');
+	*p = '\0';
+
+	// answer
+	// sscanf(buf, "left=%d", &n1);
+    // sscanf(p+1, "right=%d", &n2);
+
+	left_p = strchr(buf, '=');
+	right_p = strchr(p+1, '=');
+	
+	strcpy(arg1, left_p+1);
+	strcpy(arg2, right_p+1);
+	n1 = atoi(arg1);
+	n2 = atoi(arg2);
+    }
+
+    /* Make the response body */
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", 
+	    content, n1, n2, n1 + n2);
+    sprintf(content, "%sThanks for visiting!\r\n", content);
+  
+    /* Generate the HTTP response */
+    printf("Connection: close\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+
+    exit(0);
+}
+/* $end adder */
